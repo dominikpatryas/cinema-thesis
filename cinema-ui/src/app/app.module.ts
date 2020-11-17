22e150204b4b9a3398965dfc813ae3bb3206330c
dominikpatryas <dominik.patryas@gmail.com>
2020-11-17 18:47:01 +0100

    scss rename format and modules clean up

--- cinema-ui/src/app/app.module.ts
+++ cinema-ui/src/app/app.module.ts
@@ -7,13 +7,14 @@ import {FooterComponent} from './footer/footer.component';
 import {BackgroundWaveComponent} from './background-wave/background-wave.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {RegisterComponent} from './register/register.component';
-import {LoginComponent} from './login/login.component';
 import {NavigationComponent} from './navigation/navigation.component';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './routes';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { PriceListComponent } from './price-list/price-list.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -23,8 +24,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     BackgroundWaveComponent,
     AboutUsComponent,
     RegisterComponent,
-    LoginComponent,
-    NavigationComponent
+    NavigationComponent,
+    PriceListComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
